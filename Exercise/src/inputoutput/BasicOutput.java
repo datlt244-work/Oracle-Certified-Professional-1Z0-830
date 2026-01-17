2f2f07da8c49de369991d247bc1beff709d6851b
datlt <datlt244@gmail.com>
2026-01-17 20:21:43 +0700

    Add project structure and configuration files for Exercise
    Include .gitignore files for IntelliJ, Eclipse, and other IDEs, project configuration files (.iml, misc.xml, modules.xml, vcs.xml), and a basic Java application (BasicOutput.java) demonstrating output functionality.

---
+++ Exercise/src/inputoutput/BasicOutput.java
@@ -0,0 +1,21 @@
+package inputoutput;
+
+public class BasicOutput {
+    public static void main(String[] args){
+        System.out.println("Basic of output");
+        System.out.println("Basic of output" + " is fun");
+
+
+        System.out.println("Basic ");
+        System.out.println("of ");
+        System.out.print("output");
+        System.out.print(" is fun");
+        System.out.println();
+
+        int a = 5;
+        int b = 10;
+        int c = a + b;
+        System.out.printf("%d + %d = %d", a,b,c);
+        System.out.printf("%S", "hello");
+    }
+}
