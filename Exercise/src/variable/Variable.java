f0791a4d6fbd08422203138fc911e6d10dd59312
datlt <datlt244@gmail.com>
2026-01-17 21:36:08 +0700

    Add Variable class to demonstrate variable declaration and basic arithmetic operations
    This commit introduces a new Java class, Variable, which showcases variable declaration, valid and invalid variable names, and performs a simple addition of two integers. The implementation includes comments to guide best practices in naming conventions.

---
+++ Exercise/src/variable/Variable.java
@@ -0,0 +1,32 @@
+package variable;
+
+public class Variable {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 20;
+        int c = a+b;
+
+        System.out.println(c);
+
+        //use year instead of y
+        //use month instead of m
+
+        int age; //valid
+        int _age; //valid
+        int $age; //valid
+        int age3; //valid
+
+        /*
+        int a ge; //invalid
+        int 3age; //invalid
+         */
+
+        /*
+        int break; //invalid
+        int do; //invalid
+        int public; //invalid
+         */
+
+    }
+}
