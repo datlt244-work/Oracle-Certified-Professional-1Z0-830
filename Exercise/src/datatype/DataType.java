fc40d617f78473eecef7804ffc1f2b9aba95ffc1
datlt <datlt244@gmail.com>
2026-01-17 21:55:56 +0700

    Add DataType class to demonstrate variable declarations and data types in Java
    This commit introduces a new Java class, DataType, which showcases various data types including boolean, char, byte, short, int, long, float, and double. The implementation includes examples of variable initialization and output to the console, along with comments on string usage and formatting tips for long and float values.

---
+++ Exercise/src/datatype/DataType.java
@@ -0,0 +1,33 @@
+package datatype;
+
+public class DataType {
+    public static void main(String[] args) {
+
+        boolean a = true;
+        char b = 'R';
+        byte c = 10;
+        short d = 365;
+        int e = 6868;
+        long f = 37635534L;
+
+        float g = 352.4326F;
+        double h = 536.76743;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(h);
+
+
+        //String
+        String str1 = "My name is Dat";
+        System.out.println(str1);
+
+        //Some tips
+        long money = 434_653_476_547L;
+        float fl1= 325.645_732_26F;
+    }
+}
